e2dd7b74e7af2229ca6220789f6fd9d521880632
alexmanley <alexjmanley00@gmail.com>
2024-12-07 15:08:19 -0700

    Added functionality to nav "fixed Bug"

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 
-//nav scripts 
+//nav scripts and listeners
+window.addEventListener('resize', function() {
+    var x = document.getElementById("myLinks")
+    if (window.innerWidth > 768) 
+        x.style.display = "flex"; 
+    else
+        x.style.display = "none";
+      
+});
+
 function Toggle_Nav() {
     var x = document.getElementById("myLinks");
     
